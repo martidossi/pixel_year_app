81e6ff52a32e557ebd6df9890102689afaac9f96
Martina Dossi <martina.dossi93@gmail.com>
2023-12-24 23:56:48 +0100

    added new files

--- st_app.py
+++ st_app.py
@@ -1,11 +1,6 @@
 import streamlit as st
-from streamlit_gsheets import GSheetsConnection
 
-# Create a connection object.
-conn = st.connection("gsheets", type=GSheetsConnection)
-
-df = conn.read()
-
-# Print results.
-for row in df.itertuples():
-    st.write(f"{row.name} has a :{row.pet}:")
+import sys
+from utils import *
+sys.path.insert(0, "..")
+local_css("style.css")
