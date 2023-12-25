d03674335f9934e3ca242853e36a6a2f0ebd39b7
Martina Dossi <martina.dossi93@gmail.com>
2023-12-25 18:15:34 +0100

    Update 1_A_year_in_pixel 🟧🟨🟪.py

--- pages/1_A_year_in_pixel 🟧🟨🟪.py
+++ pages/1_A_year_in_pixel 🟧🟨🟪.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 import numpy as np
 import pandas as pd
+import matplotlib
 import matplotlib.pyplot as plt
 import seaborn as sns
 from streamlit_gsheets import GSheetsConnection
