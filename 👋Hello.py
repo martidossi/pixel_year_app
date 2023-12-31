78ae14bf39bd40ebd9cddddcab8581dd4e85f18d
Martina Dossi <martina.dossi93@gmail.com>
2023-12-31 17:52:35 +0100

    update

--- 👋Hello.py
+++ 👋Hello.py
@@ -9,16 +9,17 @@ st.set_page_config(
 
 st.sidebar.header("Martina Dossi")
 st.sidebar.subheader("🎯 Reach me out")
-st.sidebar.markdown("martinadossi.hello@gmail.com")
+st.sidebar.markdown("- martinadossi.hello@gmail.com")
+st.sidebar.markdown("- linkedin, IG, Behance")
 
 st.title("Hello!")
 
 col1, col2 = st.columns(2)
 with col1:
     st.subheader("Who I am")
-    st.markdown("nkfbkmb", unsafe_allow_html=True)
+    st.markdown("writing someth - adding sections, personal projects, blog, cv...", unsafe_allow_html=True)
     st.subheader("Why this page")
-    st.markdown("For several reasons... creating a dashboard by myself, having some kind of website/online portoflio other than Behance, doing some Python data visualization", unsafe_allow_html=True)
+    st.markdown("For several reasons...creating a dashboard by myself, having some kind of website/online portoflio other than Behance, doing some Python data visualization", unsafe_allow_html=True)
 
 with col2:
     st.image("profilepic2.jpeg")
