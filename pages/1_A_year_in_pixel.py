a547e42dfabdcdedf1b705878540e4f0deef319a
Martina Dossi <martinadossi@MacBook-Pro-di-Martina.local>
2024-05-08 23:30:05 +0200

    update

--- pages/1_A_year_in_pixel.py
+++ pages/1_A_year_in_pixel.py
@@ -1,15 +1,19 @@
 import streamlit as st
 
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import yaml
 from streamlit_gsheets import GSheetsConnection
+import streamlit.components.v1 as components
 import sys
 from utils import *
+from itertools import product
+from pywaffle import Waffle
+import plotly.express as px
+import numpy as np
 
-## Setting
+# Setting
 st.set_page_config(
     page_title="A year in pixel",
     page_icon="🟧"
@@ -19,31 +23,32 @@ sys.path.insert(0, "..")
 local_css("style.css")
 
 conn = st.connection("gsheets", type=GSheetsConnection)
+config_modebar = {'displayModeBar': False}
 
 with open("config.yml", 'r') as f:
     cfg = yaml.safe_load(f)
     
-## Sidebar
+# Sidebar
 st.sidebar.subheader('References')
-st.sidebar.image("pics/emotion-wheel.png")
 st.sidebar.markdown("""
-    The emotion wheel depicted above serves as my primary reference. Specifically, I'll be focusing on the eight internal emotions (four positives, four negatives) as my main guide, while considering the external ones to aid in distinguishing between different states. To learn more about the methodology behind this wheel, check out the links below.
-    - [The emotion wheels](https://humansystems.co/emotionwheels/)
+    The emotion wheel below represents my primary reference. Specifically, I'll be focusing on the eight internal emotions (four positives, four negatives) as my main guide, while considering the external ones to aid in distinguishing between different states. To learn more about the methodology behind this wheel, check out the links below.
+    - [Emotion wheels](https://humansystems.co/emotionwheels/)
     - [The methodology](https://www.youtube.com/watch?v=ehzj0UHIU9w&t=113s)
     """)
+with st.sidebar.expander('See the wheel:'):
+    st.image("pics/emotion-wheel.png")
 st.sidebar.subheader('Other inspirations')
 st.sidebar.markdown("""
-    - [Happy Coding Blog](https://happycoding.io/blog/year-in-pixels-2019)
+    - [Happy Coding blog](https://happycoding.io/blog/year-in-pixels-2019)
     """)
 
-## Title
+# Title
 st.title("A Year in :rainbow[Pixel]")
 st.markdown("A *Pixel Year* is a visual representation of personal emotions throughout the course of a year. Each day is assigned a specific color that refers to the predominant mood or feeling of that day.")
 
 st.subheader("**2024**")
 
-   
-## Heatmap
+# Heatmap
 dict_emotion_id = cfg['map_emotion_id']
 dict_emotion_color = cfg['map_emotion_color']
 
@@ -60,6 +65,10 @@ df_emotion = (
     .set_index('id_day')
     .fillna('0')
 )
+month_list = list(df_emotion.columns)
+emotion_list = list(dict_emotion_color.keys())[2:]
+positive_emotion_list = emotion_list[:4]
+negative_emotion_list = emotion_list[4:]
 
 # Mapping emotions to numbers
 df_emotion_id = (
@@ -139,10 +148,174 @@ with tab2:
     )
 
     ax.set_yticklabels(labels=['J', 'F', 'M', 'A', 'M', 'J', 'J', 'A', 'S', 'O', 'N', 'D'], rotation=0, fontsize=8)
-    #ax.set_xticklabels(labels=list(df_emotion_id.index), rotation=0, fontsize=8)
+    ax.set_xticklabels(labels=list(df_emotion_id.index), rotation=0, fontsize=8)
     ax.set_xlabel('')
     st.pyplot(fig)
     st.markdown("**Legenda**")
-    st.markdown("Feelings may be linked to various intensities. Cells without any smile are just neutral, whereas positive pixels may be good :) or very good :)) days, as well as negative ones can be bad :( or very bad :(( days.")
+    st.markdown("""
+    Feelings may be linked to various intensities. 
+    Cells without any smile are just neutral, whereas positive pixels may be good :) or very good :)) days,
+    as well as negative ones can be bad :( or very bad :(( days.
+    """)
 
+# Df month emotion
+df_melt = df_emotion.melt()
+df_melt = df_melt[(df_melt['value']!='0') & (df_melt['value']!='Missing')]
+df_melt.columns = ['month', 'emotion']
+df_melt['day'] = df_melt.groupby(['month']).cumcount()+1
+df_count = df_melt[['month', 'emotion']].value_counts().reset_index().rename(columns={0: 'value'})
+df_month_emotion_base = pd.DataFrame(data=product(month_list, emotion_list), columns=['month', 'emotion'])
+df_month_emotion_base['value'] = 0
+df_month_emotion = (
+    pd.merge(
+        df_month_emotion_base,
+        df_count,
+        how='left',
+        left_on=['month', 'emotion'],
+        right_on = ['month', 'emotion'])
+).fillna(0)
+df_month_emotion['value'] = df_month_emotion.value_y.combine_first(df_month_emotion.value_x)
+df_month_emotion = df_month_emotion.drop(['value_x', 'value_y'], axis=1)
 
+# Bar plot
+st.write('---')
+st.subheader('Total number of days')
+df_n_days = df_month_emotion.groupby('emotion').agg({'value': sum}).reindex(emotion_list).reset_index()
+df_n_days_pos = df_n_days[df_n_days.emotion.isin(positive_emotion_list)]
+df_n_days_neg = df_n_days[df_n_days.emotion.isin(negative_emotion_list)]
+
+df_n_days['perc_value'] = round(100*df_n_days.value/df_n_days.value.sum()).astype(int)
+df_n_days['emotion_type'] = list(np.repeat('positive emotion', 4)) + list(np.repeat('negative emotion', 4))
+
+dict_emotion_color_extended = dict_emotion_color
+dict_emotion_color_extended.update({'(?)': 'white'})
+fig = px.treemap(
+    df_n_days,
+    path=[px.Constant("all"), 'emotion_type', 'emotion'],
+    values='value',
+    color='emotion',
+    color_discrete_map=dict_emotion_color_extended,
+)
+fig.update_layout(
+    height=380,
+    margin=dict(t=20, b=0, r=50),
+)
+fig.update_traces(
+    marker=dict(cornerradius=2),
+    marker_line_width=1,
+    marker_line_color='black'
+)
+fig.data[0].textinfo = 'label+text+percent entry'
+fig.data[0].hovertemplate = '%{label}<br>number of days: %{value}'
+st.plotly_chart(fig, config=config_modebar)
+
+# Trend over time
+st.write('---')
+st.subheader('Trend over time')
+
+col1, col2 = st.columns(2)
+with col1:
+    st.markdown("""
+    Month by month, this visualization shows the trend over time of emotions, in terms of number of days.
+    Use the widget on the right to highlight selected emotions.
+    """)
+with col2:
+    sel_status = st.multiselect("Pick one or more emotions:", emotion_list, [])
+
+fig = px.line(
+    df_month_emotion,
+    x="month",
+    y="value",
+    color="emotion",
+    color_discrete_map=dict_emotion_color,
+    template="simple_white"
+)
+if len(sel_status) >= 1:
+    for d in fig['data']:
+        if d['name'] in sel_status:
+            d['line']['color'] = dict_emotion_color[d['name']]
+            d['line']['width'] = 3
+        if d['name'] not in sel_status:
+            d['line']['color'] = 'lightgrey'
+fig.update_layout(
+    font=dict(
+        family="Roboto",
+        size=12
+    ),
+    legend_title_font_color="black",
+    height=380,
+    xaxis_title="",
+    yaxis_title="number of days",
+    xaxis=dict(showgrid=True, tickcolor='black', color='black'),
+    yaxis=dict(showgrid=True, tickcolor='black', color='black'),
+    margin=dict(t=20),
+)
+fig.update_xaxes(showline=True, linewidth=1, linecolor='black')
+fig.update_yaxes(showline=True, linewidth=1, linecolor='black')
+st.plotly_chart(fig, config=config_modebar)
+
+st.markdown("##### Visualizing totals")
+tab1, tab2 = st.tabs(["Stacked bar chart", "Polar chart"])
+with tab1:
+    fig = px.bar(
+        df_month_emotion,
+        x="month",
+        y="value",
+        color="emotion",
+        color_discrete_map=dict_emotion_color,
+        template="simple_white",
+    )
+    fig.update_layout(
+        font=dict(
+            family="Roboto",
+            size=12,
+        ),
+        legend_title_font_color="black",
+        height=380,
+        xaxis_title="",
+        yaxis_title="number of days",
+        xaxis=dict(showgrid=True, tickcolor='black', color='black'),
+        yaxis=dict(showgrid=True, tickcolor='black', color='black'),
+        margin=dict(t=20),
+        bargap=0
+    )
+    fig.update_xaxes(showline=True, linewidth=1, linecolor='black')
+    fig.update_yaxes(showline=True, linewidth=1, linecolor='black')
+    st.plotly_chart(fig, config=config_modebar)
+with tab2:
+    fig = px.bar_polar(
+        df_month_emotion,
+        r="value",
+        theta="month",
+        color="emotion",
+        color_discrete_map=dict_emotion_color
+    )
+    fig.update_layout(
+        font=dict(
+            family="Roboto",
+            size=12,
+        ),
+        margin=dict(t=20),
+        height=380,
+        legend_title_font_color="black",
+        polar=dict(
+            bgcolor="white",
+            radialaxis=dict(
+                gridcolor='lightgray',
+                griddash='dot',
+                tickcolor='black',
+                tickfont_color='black'
+                ),
+            angularaxis=dict(
+                linecolor='black',
+                gridcolor='gray',
+            )
+        ),
+    )
+    st.plotly_chart(fig, config=config_modebar)
+
+with st.expander("See the data:"):
+    df_expander = (
+        pd.pivot_table(df_month_emotion, index='month', columns='emotion', values='value')[emotion_list]
+    ).reindex(month_list)
+    st.dataframe(df_expander)
--- 👋Hello.py
+++ 👋Hello.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import sys
 from utils import *
+from annotated_text import annotated_text, annotation
 
 ## Setting
 st.set_page_config(
@@ -36,23 +37,73 @@ def set_png_as_page_bg(png_file):
     return
 
 set_png_as_page_bg('pics/cover.png')
+st.sidebar.subheader('🌸 About me')
+# st.sidebar.image("pics/profile_pic.png", width=250)
+st.sidebar.markdown("""
+    I'm a data scientist with a statistical background, based in Italy,
+    and particularly passionate about data visualization. 
+    To know more about me, check out my [personal page](https://martina-dossi.notion.site/Hi-I-m-Martina-6c1e9636b59245cd8cfedc1fffd05a1b).
+    """)
+st.sidebar.subheader('💌 Contacts')
+st.sidebar.markdown("""
+    If you have any questions, feedback of any kind, I'd be delighted to connect.
+    Feel free to reach out to me!
+    - martinadossi.hello at gmail.com
+    - [LinkedIn](https://www.linkedin.com/in/martina-dossi/), [Instagram](https://www.instagram.com/adatastory_/), [Behance](https://www.behance.net/martinadossi)
+    """)
 
-st.sidebar.header("Martina Dossi")
-st.sidebar.markdown("💬 If you have any questions, feedback of any kind, I'd be delighted to connect. Feel free to reach out to me!")
-st.sidebar.markdown("- martinadossi.hello at gmail.com")
-st.sidebar.markdown("- [LinkedIn](https://www.linkedin.com/in/martina-dossi/), [Instagram](https://www.instagram.com/adatastory_/), [Behance](https://www.behance.net/martinadossi)")
+st.sidebar.subheader('📙 References')
+
+st.sidebar.markdown("""
+    *«Data collected from life can be a snapshot of the world
+    in the same way that a picture catches small moments in time.»*
+    """)
+st.sidebar.image("pics/smalldata_bigdata_output.png", width=250)
+st.sidebar.write("[Dear Data](https://www.dear-data.com/theproject), Giorgia Lupi and Stefanie Posavec.")
+st.sidebar.write("[Feltron personal annual reports](http://feltron.com/), Nicholas Felton.")
 
 st.title("Hello!")
 
-col1, col2 = st.columns(2)
-with col1:
-    st.subheader("My name is Martina.")
-    st.markdown("""
-    `why` I'd like to use this space to explore personal projects and Python visualizations, also in interactive ways. I'll be using Matplotlib, Seaborn, and Plotly.
-    
-    `who`I'm a data scientist with a statistical background, based in Italy, and particularly passionate about data visualization. To know more about me, check out my page: [URL](https://martina-dossi.notion.site/Hi-I-m-Martina-6c1e9636b59245cd8cfedc1fffd05a1b).
+st.header("My name is Martina.")
+text_1 = "Here is where I planned to visualize "
+text_2 = " I am collecting through 2024."
+st.markdown("### " + text_1 + str(annotation("personal data", "", "#fea"))
+            + text_2, unsafe_allow_html=True)
+# st.markdown("<h3>" + text + str(annotation("apple", "", "#fea")) + "</h3>", unsafe_allow_html=True)
+
+st.write("---")
+st.markdown(
+    f"""
+    → This year I am finally succeeding in collecting various personal data from everyday life (emotions, where I am, the movies I see,
+    and some others) –after a few failed attempts. Since last year, I've realized that in order to be consistent 
+    in daily data collection, I cannot gather data of too many diverse topics (if the goal is annual). 
+    So to begin with, I am focusing on what I am most interested in: {str(annotation("daily emotions", "", "#faa"))} and
+    what can influence them! 
+    """, unsafe_allow_html=True)
+st.markdown(
+    f"""
+    → If data is a filter we use to portray reality and learn more about it, then self-reporting data may serve as a 
+    powerful lens for {str(annotation("self discovery", "", "#faa"))}, to uncover and recognize patterns we might
+    not otherwise pay attention to. Taking up the concept of 
+    [Data Humanism](https://giorgialupi.com/data-humanism-my-manifesto-for-a-new-data-wold), data moves from being
+    something cold and aseptic to being filled with life, being a tool to foster human knowledge and introspection.
     """, unsafe_allow_html=True)
 
-with col2:
-    st.image("pics/profilepic2.jpeg")
+st.markdown(
+    f"""
+    → The relationships I intend to explore –aka my {str(annotation("research questions", "", "#faa"))}:
+    - How do I feel during the year? Is there a correlation between feelings and months, days, weekends?
+    - Are my emotions influenced by where I am, physically? For instance, how does going to the office affect me?
+    - Is there a correlation between watching movies and mood?
+    - How do I feel on days when I have a headache?
+    - What about cross influences? For example, days of the year and movies, cities and movies?
+    """, unsafe_allow_html=True)
+
+st.markdown(
+    f"""
+    → From a technical point of view, I use {str(annotation("Python", "", "#faa"))} to experiment with various
+    libraries, graphical models, and interactive visualizations, all supported by Streamlit.
+    """, unsafe_allow_html=True)
+
+st.subheader("Join me as I decode the year through data!")
 
